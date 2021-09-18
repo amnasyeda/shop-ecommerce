b103bb04eae1a6b8822825ab40193c1db106dde2
Amna Syeda <amna.syeda98@gmail.com>
2021-09-17 22:16:13 -0400

    added cart components

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,6 +7,7 @@ import spinner from '../assets/spinner.gif'
 
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
+import Cart from "../components/Cart";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -63,6 +64,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+            <Cart />
     </>
   );
 };
