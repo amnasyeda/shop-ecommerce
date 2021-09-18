f433a584f2354bbd166c90b481efeef81b054e8a
Amna Syeda <amna.syeda98@gmail.com>
2021-09-17 22:03:13 -0400

    working on reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,8 +1,22 @@
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-} from "./actions";
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+  } from './actions';
+
+  const defaultState = {
+    products: [],
+    categories: [],
+    currentCategory: '',
+    cart: [],
+    cartOpen: false
+}
 
 import { useReducer } from 'react';
 
@@ -28,6 +42,4 @@ export const reducer = (state, action) => {
     }
 };
 
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState)
-} 
+export default reducer;
